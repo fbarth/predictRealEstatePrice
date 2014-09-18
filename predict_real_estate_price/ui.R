c7c1c1a5685ffeb7fad291f96120173e5c51f56a
Fabricio Barth <dev+fabricio@vagas.com.br>
2014-09-18 17:07:00 -0300

    New text in ui.R

--- predict_real_estate_price/ui.R
+++ predict_real_estate_price/ui.R
@@ -2,21 +2,22 @@ shinyUI(
   fluidPage(
     # Application title
     fluidRow(
-      h1("Real Estate Prices Prediction"),
-      p("The goal of this app is predict real estate prices in Sao Paulo, Brazil."),
-      p("You can select the real estate attributes in the left menu and see the 
-        price in the right side.")
+      h1("Residential Real Estate Prices Prediction"),
+      p("The goal of this app is predict residential real estate (apartment) 
+        prices in Sao Paulo, Brazil."),
+      p("You can select the residential real estate attributes in the left 
+        menu and see the price in the right side.")
       ),    
     sidebarPanel(
-      selectInput('bairro', 'Bairro', c('paraiso',
+      selectInput('bairro', 'Neighborhood', c('paraiso',
                                         'perto_ibirapuera',
                                         'vila-mariana',                              
                                         'vila-nova-conceicao',
                                         'vila-sonia')), 
       numericInput('area', 'Area (m2)', 90),
-      numericInput('dormitorios', 'Dormitórios', 1, min = 0, max = 10, step = 1),
-      numericInput('suites', 'Suites', 1, min = 0, max = 10, step = 1),
-      numericInput('vagas', 'Vagas', 1, min = 0, max = 10, step = 1),
+      numericInput('dormitorios', 'Bedrooms', 1, min = 0, max = 10, step = 1),
+      numericInput('suites', 'Bedrooms with Bathrooms', 1, min = 0, max = 10, step = 1),
+      numericInput('vagas', 'Carspaces', 1, min = 0, max = 10, step = 1),
       submitButton('Submit')
     ),
     mainPanel(
